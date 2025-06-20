b3235ad6cc8e187a201e2085964f0f9ab8547a37
Rakib-Akanda <rmd04037@gmail.com>
2025-06-21 03:45:36 +0600

    problem solved

--- dist/app/modules/borrow/borrow.route.js
+++ dist/app/modules/borrow/borrow.route.js
@@ -1,1 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const borrow_controller_1 = __importDefault(require("./borrow.controller"));
+const borrowRoute = (0, express_1.Router)();
+borrowRoute
+    .route("/")
+    .post(borrow_controller_1.default.createBorrow);
+exports.default = borrowRoute;
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import bookRoute from "../modules/books/book.route";
+import borrowRoute from "../modules/borrow/borrow.route";
 
 const routes = Router();
 routes.use("/books", bookRoute);
-routes.use("/borrow");
+routes.use("/borrow", borrowRoute);
 
 export default routes;
